0f6e952dcd014afc46b323bdd3ded35f49e120f5
ipek <ipekcivicakan@gmail.com>
2018-12-01 10:55:12 -0800

    action creators in action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log("about to fetch posts");
+  await dispatch(fetchPosts());
+  console.log("fetched posts!");
+};
+
 export const fetchPosts = () => async dispatch => {
   //return async (dispatch, getState) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -26,12 +32,19 @@ export const fetchPosts = () => async dispatch => {
 //   });
 // };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
+//memoized version
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
